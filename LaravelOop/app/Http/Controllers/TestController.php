2c3f393b7a69b54c5c4c81087b7c3c67d8590b6d
Alberto Gambino <gambafb@libero.it>
2021-05-19 17:14:57 +0100

    Esercizio movie

---
+++ LaravelOop/app/Http/Controllers/TestController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class Movie{
+
+    public $titolo;
+    public $descrizione;
+
+    public function __construct($titolo, $descrizione = null){
+
+        $this -> titolo = $titolo;
+
+        if($descrizione = null){
+
+            $this -> descrizione = 'Nothing found';
+        } else{
+
+            $this -> descrizione = $descrizione;
+        }
+
+
+    }
+
+    public function getString(){
+
+        return "Movie title: " . $this -> titolo . "description: " . $this -> descrizione;
+    }
+}
+
+class TestController extends Controller
+{
+    public function home(){
+
+        $movie1 = new Movie('Il signore degli anelli', 'Amazing');
+
+        $movie2 = new Movie('Fearless');
+
+        $movie3 = new Movie('Avengers','Spettacolare');
+
+        $movie4 = new Movie('Fast and Furious');
+
+        $movies = [
+
+            $movie1,
+            $movie2,
+            $movie3,
+            $movie4
+        ];
+
+        $str1 = $movie1 -> getString();
+
+        $str2 = $movie2 -> getString();
+
+        $str = '';
+
+        foreach ($movies as $movie){
+
+            $str .= $movie -> getString(). "\n";
+        }
+
+         dd($str);
+
+        return view("pages.home");
+    }
+}
---
+++ LaravelOop/routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', 'TestController@home')
+    -> name('home');
